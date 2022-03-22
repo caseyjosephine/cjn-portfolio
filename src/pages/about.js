50a5feab7edb4bdbe223cb05116459e83369b687
Casey Noble <caseynoble@Starkiller-Base.local>
2022-03-22 09:08:00 -0700

    added starter content for homepage and nav

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Casey Jo Noble" pageHeading="About Casey Jo Noble">
+      <p>My name is Casey and I really like wine.</p>
+      <ul>
+          <li>🏔 I was born in a ski town in sunny Southern California</li>
+          <li>🍷 My current homebase is in Napa, CA</li>
+          <li>🌕 When I grow up I want to be a Jedi Knight</li>
+      </ul>
+    </Layout>   
+  )
+}
+
+export default AboutPage
