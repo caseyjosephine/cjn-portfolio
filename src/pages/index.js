61ceaf9c81396bc0dfee00c40d527349c7b69cd8
Casey Noble <caseynoble@Starkiller-Base.local>
2022-03-23 13:33:10 -0700

    Added image processing and fixed dependency issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Casey Jo Noble | Wine Marketing Specialist" pageHeading="Cheers 🥂 I'm Casey Jo">
       <p>I help wineries share their stories online.</p>
+      <StaticImage
+        alt="Stylized image of Casey Jo Noble savoring a glass of white wine"
+        src="../images/casey-jo-noble-napa-wine-pro.png"
+      />
       </Layout>
   )
 }
